9c9e1ce74ac7207a3677e6b74e09efd7e2f2a0a2
Marshal <m.shadmobaraki@gmail.com>
2021-04-22 00:59:43 +0430

    feat : add stlyed components theme types

--- types/styled.d.ts
+++ types/styled.d.ts
@@ -2,8 +2,54 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colors: {
+    palette: {
+      back: string;
+      onBack: string;
       primary: string;
+      onPrimary: string;
+      secondary: string;
+      onSecondary: string;
+      surface: string;
+      onSurface: string;
+      error: string;
+      onError: string;
+      success: string;
+      onSuccess: string;
+      disabled: string;
+      stroke: string;
+      overlay: string;
+    };
+    dim: {
+      1: string;
+      2: string;
+      3: string;
+      4: string;
+      5: string;
+      6: string;
+      7: string;
+      8: string;
+    };
+    defaults: {
+      fontName: string;
+    };
+    typography: {
+      headline1: string;
+      bodyMdMedium: string;
+      bodyMdNormal: string;
+      bodySmMedium: string;
+      subtitleXsNormal: string;
+      subtitleMdNormal: string;
+      infoXsMedium: string;
+    };
+    breakPoints: {
+      mobileSm: string;
+      mobileMd: string;
+      mobile: string;
+      tablet: string;
+      laptop: string;
+      laptopL: string;
+      desktop: string;
+      desktopL: string;
     };
   }
 }
