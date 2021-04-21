e591a0fd5f879370a9732422819974367874d965
Marshal <m.shadmobaraki@gmail.com>
2021-04-22 00:54:26 +0430

    feat : add device types and views

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,24 @@
-import { useTranslation } from 'next-i18next';
+// import { useTranslation } from 'next-i18next';
+// import { useSelector } from 'react-redux';
+
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-// import { useSelector } from 'react-redux';
+import { DeviceTypes } from '@/constants/device-types';
 import { getLayout } from '@/layouts/main-layout/main-layout';
+import Client from '@/views/home/client';
+import Mobile from '@/views/home/mobile';
 
-const Home = () => {
-  const { t } = useTranslation('common');
+const Home = ({ deviceType }) => (
+  // const { t } = useTranslation('common');
   // const { name } = useSelector((state) => state.test);
-  return <h1>{t('title')}</h1>;
-};
-
+  <> {deviceType === DeviceTypes.MOBILE ? <Mobile /> : <Client />}</>
+);
 Home.getLayout = getLayout;
-export const getStaticProps = async ({ locale }) => ({
+
+export default Home;
+
+export const getServerSideProps = async ({ locale }) => ({
   props: {
     ...(await serverSideTranslations(locale, ['common'])),
   },
 });
-export default Home;
