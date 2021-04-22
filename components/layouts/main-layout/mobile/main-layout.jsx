11acb89aef3694e10bd879509f489d1a0bdb3336
Marshal <m.shadmobaraki@gmail.com>
2021-04-22 13:43:38 +0430

    feat : add content

--- components/layouts/main-layout/mobile/main-layout.jsx
+++ components/layouts/main-layout/mobile/main-layout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Header from '@/layouts/main-layout/mobile/header';
+
+import * as S from './main-layout.styled';
+
 const MainLayout = ({ children }) => (
-  <div>
-    Mobile
-    {children}
-  </div>
+  <S.Wrapper>
+    <Header />
+    <S.Container>{children}</S.Container>
+  </S.Wrapper>
 );
 
 export default MainLayout;
