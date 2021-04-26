64220e07f0ed9176f60cf23966b8b580e239adca
Marshal <m.shadmobaraki@gmail.com>
2021-04-26 15:28:56 +0430

    feat : add product carousel component

---
+++ components/views/product/mobile/product-carousel/product-carousel.jsx
@@ -0,0 +1,24 @@
+import Compare from '@/assets/vectors/Compare.svg';
+import Carousel from '@/components/shared/carousel';
+import IconProvider from '@/providers/icon-provider/icon-provider';
+
+import * as S from './product-carousel.styled';
+import ProductCarouselItem from './product-carousel-item';
+
+const ProductCarousel = () => (
+  <S.Wrapper>
+    <S.IconWrap>
+      <Compare />
+    </S.IconWrap>
+    <S.IconWrap top>
+      <IconProvider icon="wishlist" size="20px" />
+    </S.IconWrap>
+    <Carousel>
+      {Array.from({ length: 5 }, (_, i) => i + 1).map((id) => (
+        <ProductCarouselItem key={id} />
+      ))}
+    </Carousel>
+  </S.Wrapper>
+);
+
+export default ProductCarousel;
