cc0d26866057a325311130a2c8ece3c22e394004
Marshal <m.shadmobaraki@gmail.com>
2021-04-26 15:35:47 +0430

    feat : add button config component

---
+++ constants/button-config.js
@@ -0,0 +1,17 @@
+export const buttonColors = {
+  ON_SURFACE: 1,
+  ERROR: 2,
+};
+
+export const buttonSizes = {
+  LARGE: 1,
+  MEDIUM: 2,
+  S_MEDIUM: 3,
+  SMALL: 4,
+};
+
+export const buttonVariants = {
+  FILL: 1,
+  OUTLINE: 2,
+  LINK: 3,
+};
