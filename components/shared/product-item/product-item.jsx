71e42521527c6552a5f10fe17cfeafdc3789c901
Marshal <m.shadmobaraki@gmail.com>
2021-04-26 15:20:43 +0430

    add product-item component

---
+++ components/shared/product-item/product-item.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import AddBag from '@/assets/vectors/AddBag.svg';
+import Toman from '@/assets/vectors/Toman.svg';
+import Typography from '@/components/shared/typography';
+import { typographyDisplay, typographyVariant } from '@/constants/typography-config';
+import IconProvider from '@/providers/icon-provider/icon-provider';
+
+import * as S from './product-item.styled';
+
+const ProductItem = ({ colors = [1, 2, 3] }) => (
+  <S.Wrap>
+    <S.Icon as={IconProvider} icon="wishlist" size="20px" />
+    <S.ImageWrap>
+      <S.Image src="/images/res/TestPic.jpg" alt="test" />
+    </S.ImageWrap>
+    <S.Bottom>
+      <S.BagIcon as={AddBag} />
+      <Typography display={typographyDisplay.BLOCK} variant={typographyVariant.HEADLINE1} gutterRight={2}>
+        {(5390000).toLocaleString('de-DE')} <Toman />
+      </Typography>
+    </S.Bottom>
+    {colors.length && (
+      <S.Colors>
+        {colors.map((id) => (
+          <S.Color key={id} id={id} />
+        ))}
+      </S.Colors>
+    )}
+  </S.Wrap>
+);
+
+export default ProductItem;
