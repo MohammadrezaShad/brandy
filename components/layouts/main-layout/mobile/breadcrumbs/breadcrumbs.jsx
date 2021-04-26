4e960442c05ce1e00374c99bba710993ab39cf21
Marshal <m.shadmobaraki@gmail.com>
2021-04-26 15:17:50 +0430

    feat : add breadcrumbs

---
+++ components/layouts/main-layout/mobile/breadcrumbs/breadcrumbs.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-array-index-key */
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import ArrowLeft from '@/assets/vectors/F-Left.svg';
+import ArrowRight from '@/assets/vectors/F-Right.svg';
+import Typography from '@/components/shared/typography';
+import { typographyVariant } from '@/constants/typography-config';
+
+import * as S from './breadcrumbs.styled';
+
+const Breadcrumbs = ({ crumbs = ['دسته‌بندی', 'دسته اول', 'دسته دوم', 'محصول ایکس'] }) => {
+  const { back } = useRouter();
+  return (
+    <S.Wrap>
+      <S.ArrowRight as={ArrowRight} onClick={() => back()} />
+      <S.Content>
+        {crumbs.map((crumb, index) => (
+          <S.Crumb key={index}>
+            {index > 0 && <S.ArrowLeft as={ArrowLeft} />}
+            <Typography variant={typographyVariant.BODY_SM_MEDIUM}>{crumb}</Typography>
+          </S.Crumb>
+        ))}
+      </S.Content>
+    </S.Wrap>
+  );
+};
+
+export default Breadcrumbs;
