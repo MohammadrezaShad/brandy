c98779c7fb52dcb88a3faa5ec4748fbe08cdf0fc
Marshal <m.shadmobaraki@gmail.com>
2021-04-29 00:20:40 +0430

    feat : card guide component

---
+++ src/components/views/card/home/mobile/card-guide/card-guide.jsx
@@ -0,0 +1,46 @@
+import Delivery from '@/assets/vectors/Delivery.svg';
+import Orginal from '@/assets/vectors/Orginal.svg';
+import Return from '@/assets/vectors/Return.svg';
+import Typography from '@/components/shared/typography';
+import { typographyColor, typographyVariant } from '@/constants/typography-config';
+
+import * as S from './card-guide.styled';
+
+const CardGuide = () => {
+  console.log('CardGuide');
+  return (
+    <S.Wrapper>
+      <Typography variant={typographyVariant.SUBTITLE_MD_NORMAL} color={typographyColor.PRIMARY} gutterBottom={2}>
+        کالاهای موجود در سبد خرید شمـا ثبت و رزرو نشـده‌اند. برای ثبت سفـارش .خود مراحل بعدی را تکمیل کنید
+      </Typography>
+      <S.Tags>
+        <S.TagWrap>
+          <S.Tag>
+            <Orginal />
+          </S.Tag>
+          <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL} color={typographyColor.PRIMARY} gutterTop={1}>
+            ضمانت اصالت کالا
+          </Typography>
+        </S.TagWrap>
+        <S.TagWrap>
+          <S.Tag>
+            <Return />
+          </S.Tag>
+          <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL} color={typographyColor.PRIMARY} gutterTop={1}>
+            ضمانت 7 روزه بازگشت کالا
+          </Typography>
+        </S.TagWrap>
+        <S.TagWrap>
+          <S.Tag>
+            <Delivery />
+          </S.Tag>
+          <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL} color={typographyColor.PRIMARY} gutterTop={1}>
+            تحویل سریع و آسان
+          </Typography>
+        </S.TagWrap>
+      </S.Tags>
+    </S.Wrapper>
+  );
+};
+
+export default CardGuide;
---
+++ src/components/views/card/home/mobile/card-guide/card-guide.styled.js
@@ -0,0 +1,28 @@
+import convert from 'color-convert';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const Tags = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: ${({ theme }) => `${theme.dim[2]} ${theme.dim[7]}`};
+  background-color: ${({ theme }) => `rgb(${convert.hex.rgb(theme.palette.strokeVariant)},0.4)`};
+  margin: ${({ theme }) => `0 -${theme.dim[2]}`};
+`;
+
+export const TagWrap = styled.div`
+  flex: 0 0 86px;
+  text-align: center;
+`;
+
+export const Tag = styled.span`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 52px;
+  height: 52px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.palette.stroke};
+`;
