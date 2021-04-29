b45f2d62021b990f9eb4d7628193e066fcfc7f5e
Marshal <m.shadmobaraki@gmail.com>
2021-04-29 22:18:17 +0430

    fix : change client and mobile component path

--- src/pages/card/info.jsx
+++ src/pages/card/info.jsx
@@ -5,8 +5,8 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { DeviceTypes } from '@/constants/device-types';
 import { getCardLayout } from '@/layouts/card-layout/card-layout';
-import Client from '@/views/card/home/client';
-import Mobile from '@/views/card/home/mobile';
+import Client from '@/views/card/info/client';
+import Mobile from '@/views/card/info/mobile';
 
 const Info = ({ deviceType }) => (
   // const { name } = useSelector((state) => state.test);
--- src/pages/card/payment.jsx
+++ src/pages/card/payment.jsx
@@ -5,8 +5,8 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { DeviceTypes } from '@/constants/device-types';
 import { getCardLayout } from '@/layouts/card-layout/card-layout';
-import Client from '@/views/card/home/client';
-import Mobile from '@/views/card/home/mobile';
+import Client from '@/views/payment/home/client';
+import Mobile from '@/views/payment/home/mobile';
 
 const Payment = ({ deviceType }) => (
   // const { name } = useSelector((state) => state.test);
