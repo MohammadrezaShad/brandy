3ecd13a9c345a581b91a39966fcc18b619486c32
Marshal <m.shadmobaraki@gmail.com>
2021-05-02 16:52:11 +0430

    feat : add button styles

--- src/components/views/card/info/mobile/info-view.stled.ts
+++ src/components/views/card/info/mobile/info-view.stled.ts
@@ -1,8 +1,18 @@
+import convert from 'color-convert';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div``;
 
 export const Wrap = styled.div`
   padding: ${({ theme }) => `${theme.dim[3]} ${theme.dim[2]}`};
-  border-top: 8px solid ${({ theme }) => theme.palette.stroke};
+  border-top: 5px solid ${({ theme }) => `rgb(${convert.hex.rgb(theme.palette.strokeVariant)},0.4)`};
+`;
+
+export const Button = styled.div`
+  position: sticky;
+  bottom: 0;
+  padding: ${({ theme }) => `${theme.dim[2]}`};
+  border-top: 1px solid ${({ theme }) => theme.palette.stroke};
+  background-color: ${({ theme }) => theme.palette.back};
+  z-index: 100;
 `;
