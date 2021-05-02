f185e7feacacf0f4f59072faa644260aecc5a5b0
Marshal <m.shadmobaraki@gmail.com>
2021-05-02 16:44:46 +0430

    feat : add radio button component

---
+++ src/components/shared/form/radio/radio.styled.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.label`
+  display: flex;
+  align-items: center;
+`;
+
+export const Layout = styled.span<{ checked: boolean }>`
+  display: inline-flex;
+  position: relative;
+  flex: 0 0 16px;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  border: 2px solid ${({ theme }) => theme.palette.onSuccess};
+  transition: all 0.5s;
+  &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    right: 50%;
+    transform: translate(50%, -50%);
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: ${({ theme, checked }) => (checked ? theme.palette.onSuccess : 'transparent')};
+    transition: all 0.3s;
+  }
+`;
+
+export const Input = styled.input`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  z-index: 10;
+`;
---
+++ src/components/shared/form/radio/radio.tsx
@@ -0,0 +1,24 @@
+import { FC, FormEvent } from 'react';
+
+import * as S from './radio.styled';
+
+type RadioProps = {
+  children?: unknown;
+  onChange: (event: FormEvent<HTMLInputElement>) => void;
+  checked?: boolean;
+  name?: string;
+};
+
+const Radio: FC<RadioProps> = ({ children, onChange, name, checked }) => {
+  console.log('Radio');
+  return (
+    <S.Wrap>
+      <S.Layout checked={checked}>
+        <S.Input type="radio" checked={checked} name={name} onChange={onChange} />
+      </S.Layout>
+      {children}
+    </S.Wrap>
+  );
+};
+
+export default Radio;
