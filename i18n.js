fdfb36ea5dd0bb1e4638b008044b2660be92b113
Marshal <m.shadmobaraki@gmail.com>
2021-05-03 16:47:14 +0430

    feat : add i18n

---
+++ i18n.js
@@ -0,0 +1,11 @@
+module.exports = {
+  locales: ['fa'],
+  defaultLocale: 'fa',
+  pages: {
+    '*': ['common'],
+  },
+  loadLocaleFrom: (lang, ns) =>
+    // You can use a dynamic import, fetch, whatever. You should
+    // return a Promise with the JSON file.
+    import(`./public/locales/${lang}/${ns}.json`).then((m) => m.default),
+};
