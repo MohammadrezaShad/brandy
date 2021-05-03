81a0197087ec66e3fe97ec1bf6d677ae16638356
Marshal <m.shadmobaraki@gmail.com>
2021-05-03 16:51:21 +0430

    add : convert js to typescript file

---
+++ src/redux/root-reducer.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import testReducer from './slices/test-slice';
+
+const rootReducer = combineReducers({
+  test: testReducer,
+});
+
+export default rootReducer;
---
+++ src/redux/store.ts
@@ -0,0 +1,23 @@
+import { configureStore, EnhancedStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { createWrapper, MakeStore } from 'next-redux-wrapper';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import rootReducer from './root-reducer';
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const middlewares = [...getDefaultMiddleware<RootState>()];
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: middlewares,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export const makeStore: MakeStore = (): EnhancedStore => store;
+
+export const reduxWrap = createWrapper(makeStore);
+
+type AppDispatch = typeof store.dispatch;
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
