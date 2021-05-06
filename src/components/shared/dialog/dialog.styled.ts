15edec2cf5649729be394f2f35d97515dd562660
Marshal <m.shadmobaraki@gmail.com>
2021-05-06 16:54:57 +0430

    feat : add dialog component

---
+++ src/components/shared/dialog/dialog.styled.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.section`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 1000;
+  overflow: auto;
+`;
+
+export const Layout = styled.div`
+  position: relative;
+  padding: ${({ theme }) => theme.dim[2]};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  min-height: 100%;
+`;
+
+export const Container = styled.div`
+  position: relative;
+  z-index: 5;
+  max-width: 100%;
+`;
+
+export const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.palette.overlay};
+  opacity: 0.5;
+  z-index: 1;
+`;
---
+++ src/components/shared/dialog/dialog.tsx
@@ -0,0 +1,57 @@
+import { FC, ReactChild, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+import { disableBodyScroll, enableBodyScroll } from '@/utils/body-scroll';
+import { canUseDOM } from '@/utils/can-use-dom';
+
+import * as S from './dialog.styled';
+
+type DialogProps = {
+  open: boolean;
+  onClose: () => void;
+  children: unknown | string | ReactChild;
+};
+
+const Dialog: FC<DialogProps> = ({ open, onClose, children }) => {
+  const [isDialogShow, setIsDialogShow] = useState<boolean>(false);
+
+  const handleCloseDialog = (): void => {
+    setIsDialogShow(false);
+    onClose();
+  };
+
+  useEffect(() => {
+    setIsDialogShow(open);
+
+    if (open) {
+      disableBodyScroll();
+    } else {
+      enableBodyScroll();
+    }
+
+    return () => {
+      if (isDialogShow) {
+        enableBodyScroll();
+      }
+    };
+  }, [open, isDialogShow]);
+
+  if (!canUseDOM()) {
+    return null;
+  }
+
+  if (!isDialogShow) {
+    return null;
+  }
+  return createPortal(
+    <S.Wrap>
+      <S.Layout>
+        <S.Backdrop onClick={handleCloseDialog} />
+        <S.Container>{children}</S.Container>
+      </S.Layout>
+    </S.Wrap>,
+    typeof document !== 'undefined' ? document.getElementById('portal') : null,
+  );
+};
+
+export default Dialog;
