6e75b5acdcc4eb11ea412eac3c96b7b8e54bd93c
Marshal <m.shadmobaraki@gmail.com>
2021-05-06 17:10:44 +0430

    perf : add usecallback and get body offset

--- src/helpers/use-scroll.ts
+++ src/helpers/use-scroll.ts
@@ -1,30 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+
+type BodyRect = {
+  top: number;
+  left: number;
+};
 
 export function useScroll() {
-  const [lastScrollTop, setLastScrollTop] = useState(0);
-  const [bodyOffset, setBodyOffset] = useState(
-    typeof window === 'undefined' || !window.document ? { top: 0, left: 0 } : document.body.getBoundingClientRect(),
-  );
-  const [scrollY, setScrollY] = useState(bodyOffset.top);
-  const [scrollX, setScrollX] = useState(bodyOffset.left);
+  const [lastScrollTop, setLastScrollTop] = useState<number>(0);
+  const [bodyOffset, setBodyOffset] = useState<DOMRect | BodyRect>(getBodyOffset());
+  const [scrollY, setScrollY] = useState<number>(bodyOffset.top);
+  const [scrollX, setScrollX] = useState<number>(bodyOffset.left);
   const [scrollDirection, setScrollDirection] = useState<undefined | string>();
 
-  const handleScroll = () => {
-    setBodyOffset(
-      typeof window === 'undefined' || !window.document ? { top: 0, left: 0 } : document.body.getBoundingClientRect(),
-    );
+  const handleScroll = useCallback(() => {
+    setBodyOffset(getBodyOffset());
     setScrollY(-bodyOffset.top);
     setScrollX(bodyOffset.left);
     setScrollDirection(lastScrollTop > -bodyOffset.top ? 'down' : 'up');
     setLastScrollTop(-bodyOffset.top);
-  };
+  }, [lastScrollTop, bodyOffset.top, bodyOffset.left]);
+
+  function getBodyOffset(): DOMRect | BodyRect {
+    return typeof window === 'undefined' || !window.document
+      ? { top: 0, left: 0 }
+      : document.body.getBoundingClientRect();
+  }
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  });
+  }, [handleScroll]);
 
   return {
     scrollY,
