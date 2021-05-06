f79ef915da6a3f2a2a3b2420a08ba81b4124bd00
Marshal <m.shadmobaraki@gmail.com>
2021-05-06 17:16:25 +0430

    feat : add brand product component

---
+++ src/components/views/brand/mobile/brand-product/brand-product.styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  overflow: hidden;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: ${({ theme }) => `-${(parseInt(theme.dim[2], 10) * 3) / 4}px -${theme.dim[1]}`};
+`;
+
+export const Product = styled.div`
+  flex: 0 0 50%;
+  padding: ${({ theme }) => `${(parseInt(theme.dim[2], 10) * 3) / 4}px ${theme.dim[1]}`};
+`;
