10503716f833edf911bce7859e0ea1cf19a0c3f5
Marshal <m.shadmobaraki@gmail.com>
2021-05-08 22:41:27 +0430

    feat : add devicetype prop

--- src/components/shared/scroll-carousel/scroll-carousel.tsx
+++ src/components/shared/scroll-carousel/scroll-carousel.tsx
@@ -1,15 +1,18 @@
 import { Children, FC, useEffect, useState } from 'react';
 
+import { DeviceTypes } from '@/constants/device-types';
+import { ValueOf } from '@/types/main';
 import { isTouchDevice as isTouchDeviceMethod } from '@/utils/is-touch-device';
 
 import * as S from './scroll-carousel.styled';
 
 type ScrollCarouselProp = {
   children: unknown;
+  deviceType?: ValueOf<typeof DeviceTypes>;
 };
 
-const ScrollCarousel: FC<ScrollCarouselProp> = ({ children }) => {
-  const [isTouchDevice, setIsTouchDevice] = useState<boolean>(false);
+const ScrollCarousel: FC<ScrollCarouselProp> = ({ children, deviceType }) => {
+  const [isTouchDevice, setIsTouchDevice] = useState<boolean>(false || deviceType === DeviceTypes.MOBILE);
   const carouselSlides = Children.toArray(children);
   useEffect(() => {
     setIsTouchDevice(isTouchDeviceMethod);
