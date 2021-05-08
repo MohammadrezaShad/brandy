d2f79093d4d50dcdab31dda113cf910eb745cc35
Marshal <m.shadmobaraki@gmail.com>
2021-05-08 22:44:07 +0430

    feat : add category page

---
+++ src/pages/category/[id].tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from 'next';
+import { DeviceType, ReactComponent } from 'src/types/main';
+
+import { DeviceTypes } from '@/constants/device-types';
+import { getLayout } from '@/layouts/main-layout/main-layout';
+import Client from '@/views/category/client';
+import Mobile from '@/views/category/mobile';
+
+type CategoryProps = {
+  deviceType: DeviceType;
+};
+
+const Category: ReactComponent<CategoryProps> = ({ deviceType }) => (
+  <> {deviceType === DeviceTypes.MOBILE ? <Mobile /> : <Client />}</>
+);
+
+export default Category;
+
+Category.getLayout = getLayout;
+
+export const getServerSideProps: GetServerSideProps = async () => ({
+  props: {},
+});
