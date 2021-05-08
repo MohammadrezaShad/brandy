ec0abe5a466b2d7aacfb9c8a1c89564a1d4727cf
Marshal <m.shadmobaraki@gmail.com>
2021-05-08 22:42:32 +0430

    feat : add devicetype context

--- src/components/views/brand/mobile/brand-category/brand-category.tsx
+++ src/components/views/brand/mobile/brand-category/brand-category.tsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
+
+import DeviceTypeContext from '@/context/device-type-context';
 import ScrollCarousel from '@/shared/scroll-carousel';
 import Typography from '@/shared/typography';
 
 import * as S from './brand-category.styled';
 
 const BrandCategory = () => {
-  console.log('BrandCategory');
+  const { deviceType } = useContext(DeviceTypeContext);
+
   return (
     <S.Wrapper>
-      <ScrollCarousel>
+      <ScrollCarousel deviceType={deviceType}>
         {Array.from({ length: 13 }, (_, i) => i + 1).map((id) => (
           <S.CategoryWrap key={id}>
             <S.Category />
