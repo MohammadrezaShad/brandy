505f0e18beb5e1c4a5540f8ea09801bb3deac089
Marshal <m.shadmobaraki@gmail.com>
2021-05-17 23:23:27 +0430

    refactor : format syntax

--- src/components/layouts/card-layout/mobile/card-progress/card-progress.styled.ts
+++ src/components/layouts/card-layout/mobile/card-progress/card-progress.styled.ts
@@ -6,46 +6,53 @@ export const Wrapper = styled.section``;
 export const Wrap = styled.div`
   display: flex;
   align-items: center;
-  gap: ${({ theme }) => theme.dim[2]};
+  gap: ${({theme}) => theme.dim[2]};
   width: 100%;
   justify-content: space-between;
-  padding-bottom: ${({ theme }) => theme.dim[2]};
+  padding-bottom: ${({theme}) => theme.dim[2]};
 `;
 
-export const Icon = styled.div<{ selected: boolean }>`
+export const Icon = styled.div<{selected: boolean}>`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 50px;
   flex: 0 0 50px;
   border: 1px solid
-    ${({ theme, selected }) =>
-      selected ? theme.palette.onSuccess : `rgb(${convert.hex.rgb(theme.palette.onSuccess)},0.2)`};
-  border-radius: ${({ theme }) => theme.defaults.borderRadius};
-  background-color: ${({ theme, selected }) =>
-    selected ? theme.palette.onSuccess : `rgb(${convert.hex.rgb(theme.palette.onSuccess)},0.05)`};
+    ${({theme, selected}) =>
+      selected
+        ? theme.palette.onSuccess
+        : `rgb(${convert.hex.rgb(theme.palette.onSuccess)},0.2)`};
+  border-radius: ${({theme}) => theme.defaults.borderRadius};
+  background-color: ${({theme, selected}) =>
+    selected
+      ? theme.palette.onSuccess
+      : `rgb(${convert.hex.rgb(theme.palette.onSuccess)},0.05)`};
   transition: all 0.5s;
   position: relative;
   svg {
     transition: all 0.5s;
     PATH {
       transition: all 0.5s;
-      fill: ${({ theme, selected }) => (selected ? theme.palette.onSecondary : theme.palette.onSuccess)};
+      fill: ${({theme, selected}) =>
+        selected ? theme.palette.onSecondary : theme.palette.onSuccess};
     }
   }
 `;
 
-export const Line = styled.div<{ selected: boolean }>`
+export const Line = styled.div<{selected: boolean}>`
   height: 1px;
   flex: 1;
-  background-color: ${({ theme, selected }) =>
-    selected ? theme.palette.onSuccess : `rgb(${convert.hex.rgb(theme.palette.onSuccess)},0.2)`};
+  background-color: ${({theme, selected}) =>
+    selected
+      ? theme.palette.onSuccess
+      : `rgb(${convert.hex.rgb(theme.palette.onSuccess)},0.2)`};
   transition: all 0.5s;
 `;
 
 export const Text = styled.span`
   position: absolute;
-  bottom: ${({ theme }) => `-${theme.dim[4]}`};
+  bottom: ${({theme}) => `-${theme.dim[4]}`};
   display: block;
   width: max-content;
   transform: translateX(50%);
