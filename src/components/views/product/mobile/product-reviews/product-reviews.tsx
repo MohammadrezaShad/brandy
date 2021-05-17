5734275dda2a6440f58ac18504a40fa6c40ced55
Marshal <m.shadmobaraki@gmail.com>
2021-05-17 18:11:16 +0430

    refactor : format syntax

--- src/components/views/product/mobile/product-reviews/product-reviews.tsx
+++ src/components/views/product/mobile/product-reviews/product-reviews.tsx
@@ -1,5 +1,5 @@
 import useTranslation from 'next-translate/useTranslation';
-import { FC, useContext } from 'react';
+import {FC, useContext} from 'react';
 
 import Tick from '@/assets/vectors/Tick.svg';
 import Time from '@/assets/vectors/Time.svg';
@@ -7,38 +7,56 @@ import Button from '@/components/shared/button/button';
 import ScrollCarousel from '@/components/shared/scroll-carousel/scroll-carousel';
 import StarRating from '@/components/shared/star-rating/star-rating';
 import Typography from '@/components/shared/typography';
-import { buttonColors, buttonSizes } from '@/constants/button-config';
-import { typographyColor, typographyVariant } from '@/constants/typography-config';
+import {buttonColors, buttonSizes} from '@/constants/button-config';
+import {
+  typographyColor,
+  typographyVariant,
+} from '@/constants/typography-config';
 import DeviceTypeContext from '@/context/device-type-context';
 
 import * as S from './product-reviews.styled';
 
 const ProductReviews: FC = () => {
-  const { t } = useTranslation('common');
-  const { deviceType } = useContext(DeviceTypeContext);
+  const {t} = useTranslation('common');
+  const {deviceType} = useContext(DeviceTypeContext);
 
   return (
     <>
       <S.Wrap>
         <S.Content>
-          <Typography variant={typographyVariant.BODY_SM_MEDIUM} gutterBottom={3}>
+          <Typography
+            variant={typographyVariant.BODY_SM_MEDIUM}
+            gutterBottom={3}
+          >
             {t('sizeQuestion')}
           </Typography>
 
           <S.TextWrap>
-            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>کوچـک</Typography>
-            <S.RateProgress progress="25%" />
-            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>25%</Typography>
+            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>
+              کوچـک
+            </Typography>
+            <S.RateProgress progress='25%' />
+            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>
+              25%
+            </Typography>
           </S.TextWrap>
           <S.TextWrap>
-            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>مناسب</Typography>
-            <S.RateProgress secondary progress="25%" />
-            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>25%</Typography>
+            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>
+              مناسب
+            </Typography>
+            <S.RateProgress secondary progress='25%' />
+            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>
+              25%
+            </Typography>
           </S.TextWrap>
           <S.TextWrap>
-            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>بزرگ</Typography>
-            <S.RateProgress progress="50%" />
-            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>50%</Typography>
+            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>
+              بزرگ
+            </Typography>
+            <S.RateProgress progress='50%' />
+            <Typography variant={typographyVariant.SUBTITLE_XS_NORMAL}>
+              50%
+            </Typography>
           </S.TextWrap>
         </S.Content>
         <S.Rate>
@@ -53,14 +71,19 @@ const ProductReviews: FC = () => {
       </S.Wrap>
       <S.Comments>
         <ScrollCarousel deviceType={deviceType}>
-          {Array.from({ length: 5 }, (_, i) => i + 1).map((id) => (
+          {Array.from({length: 5}, (_, i) => i + 1).map(id => (
             <S.Comment key={id}>
               <S.CommentHead>
-                <Typography variant={typographyVariant.BODY_MD_MEDIUM}>RE***@Gmail.com</Typography>
+                <Typography variant={typographyVariant.BODY_MD_MEDIUM}>
+                  RE***@Gmail.com
+                </Typography>
                 <StarRating rate={4.7} />
               </S.CommentHead>
               <S.CommentBody>
-                <Typography color={typographyColor.ON_PRIMARY} variant={typographyVariant.SUBTITLE_MD_NORMAL}>
+                <Typography
+                  color={typographyColor.ON_PRIMARY}
+                  variant={typographyVariant.SUBTITLE_MD_NORMAL}
+                >
                   نظر ثبت شده صرفا جنبه نمایشی دارد. این نظر، صرفا جنبه
                 </Typography>
               </S.CommentBody>
