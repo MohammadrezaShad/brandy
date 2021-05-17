df61219176ce9718595458c9f8dbd1ecdb1508dc
Marshal <m.shadmobaraki@gmail.com>
2021-05-17 17:55:31 +0430

    refactor : format syntax

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,16 +1,55 @@
 /* eslint-disable no-unused-vars */
-import { FC, ReactChild } from 'react';
+import {FC, ReactChild} from 'react';
 
-import { DeviceTypes } from '@/constants/device-types';
-import { ThemeTypes } from '@/constants/theme-types';
+import {DeviceTypes} from '@/constants/device-types';
+import {ThemeTypes} from '@/constants/theme-types';
 
-export type GetLayout = (page: ReactChild, deviceType?: DeviceType) => JSX.Element;
+export type GetLayout = (
+  page: ReactChild,
+  deviceType?: DeviceType,
+) => JSX.Element;
 export type DeviceType = typeof DeviceTypes.MOBILE | typeof DeviceTypes.DESKTOP;
 export type ThemeType = typeof ThemeTypes.DARK | typeof ThemeTypes.LIGHT;
-export type ReactComponent<T> = FC<T> & { getLayout: GetLayout };
+export type ReactComponent<T> = FC<T> & {getLayout: GetLayout};
 export type LayoutProps = {
   children: unknown;
   deviceType: DeviceType;
 };
 
 export type ValueOf<T> = T[keyof T];
+
+export type Filter = {
+  id: number;
+  checked: boolean;
+  title: string;
+  color?: string;
+};
+
+export type FiltersProps = {
+  sizingFilters: Filter[];
+  templateFilters: Filter[];
+  coloringFilters: Filter[];
+  minPrice: number;
+  maxPrice: number;
+  handleChangeSizingFilter: (
+    filterId: number,
+    event: ChangeEvent<HTMLInputElement>,
+  ) => void;
+  handleChangeTemplateFilter: (
+    filterId: number,
+    event: ChangeEvent<HTMLInputElement>,
+  ) => void;
+  handleChangeColoringFilter: (
+    filterId: number,
+    event: ChangeEvent<HTMLInputElement>,
+  ) => void;
+  resetFilter: boolean;
+  renderActiveFiltersStatus: (filters: Filter[]) => string | number;
+  removeActiveColoringFilter: (filterId: number) => void;
+  removeActiveSizingFilter: (filterId: number) => void;
+  removeActiveTemplateFilter: (filterId: number) => void;
+  getActiveFilters: (filters: Filter[]) => Filter[];
+  changeMinPriceAction: (coefficient: number) => void;
+  changeMaxPriceAction: (coefficient: number) => void;
+  removeAllfilters: () => void;
+};
