18a4b80edac3243fb4ee78f179d81217c48cb2f5
Marshal <m.shadmobaraki@gmail.com>
2021-05-17 23:24:04 +0430

    feat : add block

--- src/components/views/brand/mobile/brand-banner/brand-banner.styled.ts
+++ src/components/views/brand/mobile/brand-banner/brand-banner.styled.ts
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  padding: ${({theme}) => `${theme.dim[1]} ${theme.dim[3]}`};
+  background-color: ${({theme}) => theme.palette.error};
+`;
+
+export const Block = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: ${({ theme }) => `${theme.dim[1]} ${theme.dim[3]}`};
-  background-color: ${({ theme }) => theme.palette.error};
+  position: relative;
+  &:not(:last-child) {
+    margin-bottom: ${({theme}) => theme.dim[2]};
+  }
 `;
 
 export const Wrap = styled.div`
@@ -13,12 +20,19 @@ export const Wrap = styled.div`
   align-items: center;
 `;
 
+export const Magnify = styled.span`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: ${({theme}) => theme.dim[2]};
+`;
+
 export const Arrow = styled.span`
   display: inline-flex;
-  margin-left: ${({ theme }) => theme.dim[2]};
+  margin-left: ${({theme}) => theme.dim[2]};
   transform: scale(1.2);
   & > PATH {
-    stroke: ${({ theme }) => theme.palette.back};
+    stroke: ${({theme}) => theme.palette.back};
   }
 `;
 
@@ -26,11 +40,11 @@ export const Point = styled.span`
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  margin-left: ${({ theme }) => theme.dim[1]};
-  ${({ theme }) => theme.typography.infoXsMedium};
-  color: ${({ theme }) => theme.palette.onSecondary};
-  background-color: ${({ theme }) => theme.palette.secondary};
-  border-radius: ${({ theme }) => theme.defaults.borderRadius};
+  margin-left: ${({theme}) => theme.dim[1]};
+  ${({theme}) => theme.typography.infoXsMedium};
+  color: ${({theme}) => theme.palette.onSecondary};
+  background-color: ${({theme}) => theme.palette.secondary};
+  border-radius: ${({theme}) => theme.defaults.borderRadius};
   padding-top: 3px;
   height: 13px;
   width: 26px;
@@ -40,9 +54,9 @@ export const Brand = styled.span`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  margin-right: ${({ theme }) => theme.dim[2]};
+  margin-right: ${({theme}) => theme.dim[2]};
   height: 40px;
   width: 40px;
   border-radius: 50%;
-  background-color: ${({ theme }) => theme.palette.back};
+  background-color: ${({theme}) => theme.palette.back};
 `;
--- src/components/views/brand/mobile/brand-banner/brand-banner.tsx
+++ src/components/views/brand/mobile/brand-banner/brand-banner.tsx
@@ -1,28 +1,51 @@
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 
 import Arrow from '@/assets/vectors/F-Right.svg';
-import { typographyColor, typographyVariant } from '@/constants/typography-config';
+import Magnify from '@/assets/vectors/Magnify.svg';
+import {
+  typographyColor,
+  typographyVariant,
+} from '@/constants/typography-config';
+import Input from '@/shared/form/input';
 import Typography from '@/shared/typography';
 
 import * as S from './brand-banner.styled';
 
 const BrandBanner = () => {
-  const { back } = useRouter();
+  const {back} = useRouter();
   return (
     <S.Wrapper>
-      <S.Wrap>
-        <S.Arrow as={Arrow} onClick={() => back()} />
-        <Typography color={typographyColor.ON_SECONDARY} variant={typographyVariant.BODY_SM_MEDIUM}>
-          تیشرت و پولوشرت
-        </Typography>
-      </S.Wrap>
-      <S.Wrap>
-        <S.Point>9.3</S.Point>
-        <Typography color={typographyColor.ON_SECONDARY} variant={typographyVariant.BODY_MD_MEDIUM}>
-          ZARA
-        </Typography>
-        <S.Brand />
-      </S.Wrap>
+      <S.Block>
+        <S.Wrap>
+          <S.Arrow as={Arrow} onClick={() => back()} />
+          <Typography
+            color={typographyColor.ON_SECONDARY}
+            variant={typographyVariant.BODY_SM_MEDIUM}
+          >
+            تیشرت و پولوشرت
+          </Typography>
+        </S.Wrap>
+        <S.Wrap>
+          <S.Point>9.3</S.Point>
+          <Typography
+            color={typographyColor.ON_SECONDARY}
+            variant={typographyVariant.BODY_MD_MEDIUM}
+          >
+            ZARA
+          </Typography>
+          <S.Brand />
+        </S.Wrap>
+      </S.Block>
+      <S.Block>
+        <S.Magnify as={Magnify} />
+        <Input
+          handleChange={() => {
+            console.log('red');
+          }}
+          placeholder='جستجو در علاقه‌مندی‌ها'
+          paddingRight={5}
+        />
+      </S.Block>
     </S.Wrapper>
   );
 };
