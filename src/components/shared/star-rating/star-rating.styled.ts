38f3830b5664d156698035eeceeb21bfaa17c1df
Marshal <m.shadmobaraki@gmail.com>
2021-05-17 17:43:02 +0430

    refactor : format syntax

--- src/components/shared/star-rating/star-rating.styled.ts
+++ src/components/shared/star-rating/star-rating.styled.ts
@@ -2,4 +2,26 @@ import styled from 'styled-components';
 
 export const Wrap = styled.div`
   direction: ltr;
+  position: relative;
+`;
+
+export const Container = styled.div<{starRate: number}>`
+  position: absolute;
+  overflow: hidden;
+  top: 0;
+  width: ${({starRate}) => `${(starRate / 5) * 100}%`};
+  display: flex;
+`;
+
+export const StarWrap = styled.span``;
+
+export const Wrapper = styled.div`
+  background-color: red;
+  position: absolute;
+  overflow: hidden;
+  top: 0;
+  width: 100%;
+  bottom: 0;
+  z-index: 10;
+  background-color: transparent;
 `;
--- src/components/shared/star-rating/star-rating.tsx
+++ src/components/shared/star-rating/star-rating.tsx
@@ -1,23 +1,47 @@
 /* eslint-disable no-nested-ternary */
-import { FC } from 'react';
+import { FC, MouseEvent, useState } from 'react';
 
 import EmptyStar from '@/assets/vectors/EStar.svg';
 import FullStar from '@/assets/vectors/FStar.svg';
-import HalfStar from '@/assets/vectors/HStar.svg';
 
 import * as S from './star-rating.styled';
 
 type StarRatingProps = {
   rate: number;
+  readOnly?: boolean;
+  getChangedRate?: (newRate: number) => void;
 };
-const StarRating: FC<StarRatingProps> = ({ rate = 4.5 }) => (
-  <S.Wrap>
-    {Array.from({ length: 5 }, (_, i) => i + 1).map((id) => (
-      <span key={id}>
-        {id + 1 <= rate ? <FullStar key={id} /> : id + 0.5 <= rate ? <HalfStar /> : <EmptyStar key={id} />}
-      </span>
-    ))}
-  </S.Wrap>
-);
+const StarRating: FC<StarRatingProps> = ({ rate = 0, readOnly = true, getChangedRate }) => {
+  const [starRate, setStarRate] = useState(rate);
+
+  const handleMouseClick = (event: MouseEvent<HTMLSpanElement>) => {
+    const calculatedRate =
+      ((event.clientX - event.currentTarget.getClientRects()[0].left) / event.currentTarget.offsetWidth) * 5;
+    const formattedRate =
+      Math.ceil(calculatedRate) - calculatedRate < 0.1
+        ? Math.ceil(calculatedRate)
+        : Math.round(calculatedRate * 10) / 10;
+    setStarRate(formattedRate);
+    getChangedRate && getChangedRate(formattedRate);
+  };
+
+  return (
+    <S.Wrap>
+      <S.Wrapper onClick={readOnly ? null : handleMouseClick} />
+      {Array.from({ length: 5 }, (_, i) => i + 1).map((id) => (
+        <S.StarWrap key={id}>
+          <EmptyStar key={id} />
+        </S.StarWrap>
+      ))}
+      <S.Container starRate={starRate}>
+        {Array.from({ length: 5 }, (_, i) => i + 1).map((id) => (
+          <S.StarWrap key={id}>
+            <FullStar key={id} />
+          </S.StarWrap>
+        ))}
+      </S.Container>
+    </S.Wrap>
+  );
+};
 
 export default StarRating;
