b2b60e8c7fba97c3d4fe8049f74336b8f7255f89
Marshal <m.shadmobaraki@gmail.com>
2021-06-02 19:07:44 +0430

    feat: add product comment dialog

--- src/components/views/comments/home/mobile/home-view.tsx
+++ src/components/views/comments/home/mobile/home-view.tsx
@@ -1,4 +1,6 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
+
+import ProductComment from '@/dialogs/comments/product-comment';
 
 import CommentsBottom from './comments-bottom';
 import CommentsHead from './comments-head';
@@ -6,17 +8,32 @@ import CommentsList from './comments-list';
 import * as S from './home-view.styled';
 import ProductDetail from './product-detail';
 
-const HomeView: FC = () => (
-  <S.Wrapper>
-    <CommentsHead />
-    <S.Detail>
-      <ProductDetail />
-    </S.Detail>
-    <S.Block>
-      <CommentsList />
-    </S.Block>
-    <CommentsBottom />
-  </S.Wrapper>
-);
+const HomeView: FC = () => {
+  const [isCommentDialogOpen, setIsCommentDialogOpen] = useState(false);
+
+  const handleCloseCommentDialog = () => {
+    setIsCommentDialogOpen(false);
+  };
+
+  const handleOpenCommentDialog = () => {
+    setIsCommentDialogOpen(true);
+  };
+  return (
+    <S.Wrapper>
+      <CommentsHead />
+      <S.Detail>
+        <ProductDetail />
+      </S.Detail>
+      <S.Block>
+        <CommentsList />
+      </S.Block>
+      <CommentsBottom handleOpenCommentDialog={handleOpenCommentDialog} />
+      <ProductComment
+        open={isCommentDialogOpen}
+        onClose={handleCloseCommentDialog}
+      />
+    </S.Wrapper>
+  );
+};
 
 export default HomeView;
