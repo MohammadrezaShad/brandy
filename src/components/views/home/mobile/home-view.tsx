6cb2e404da8b85d56cc0f03b719b2b77c377e13b
Marshal <m.shadmobaraki@gmail.com>
2021-06-05 00:34:00 +0430

    feat : add content

--- src/components/views/home/mobile/home-view.tsx
+++ src/components/views/home/mobile/home-view.tsx
@@ -1,5 +1,19 @@
 import React, {FC} from 'react';
 
-const HomeView: FC = () => <div>MobileHomeView</div>;
+import HomeCategories from './home-categories';
+import HomeProductsCategory from './home-products-category';
+import HomeProductsList from './home-products-list';
+import HomeSpecialOffer from './home-special-offer';
+import HomeTop from './home-top/home-top';
+
+const HomeView: FC = () => (
+  <div>
+    <HomeTop />
+    <HomeSpecialOffer />
+    <HomeCategories />
+    <HomeProductsCategory />
+    <HomeProductsList />
+  </div>
+);
 
 export default HomeView;
